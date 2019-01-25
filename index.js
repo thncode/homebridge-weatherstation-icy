fdf344028dfd3d67de22e4d5ec14902be8ce926b
Thomas Nemec <30568242+thncode@users.noreply.github.com>
2019-01-25 07:06:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-var Service, Characteristic, HomebridgeAPI, FakeGatoHistoryService;
+var Service, Characteristic, HomebridgeAPI, UUIDGen, FakeGatoHistoryService;
 var inherits = require('util').inherits;
 var os = require("os");
 var hostname = os.hostname();
 const fs = require('fs');
 const moment = require('moment');
 
-
-var intervalID;
-
 const readFile = "/home/pi/WeatherStation/data.txt";
 
-var maxWind;
-var avgWind;
-var battery;
-
-var alertLevel;
-
-var glog;
-var ctime;
-
+var temperature, battery, alertLevel, readtime, icy, wasIcy;
 var lastActivation, lastReset, lastChange, timesOpened, timeOpen, timeClose;
 
 module.exports = function (homebridge) {
@@ -26,90 +15,95 @@ module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     HomebridgeAPI = homebridge;
+    UUIDGen = homebridge.hap.uuid;
     FakeGatoHistoryService = require("fakegato-history")(homebridge);
 
     homebridge.registerAccessory("homebridge-weatherstation-icy", "WeatherStationIcy", WeatherStationIcy);
 };
 
 
-function read() {
-	var data = fs.readFileSync(readFile, "utf-8");
-	var lastSync = Date.parse(data.substring(0, 19));
-	temperature = parseFloat(data.substring(20));
-	battery = parseFloat(data.substring(57));
-}
-
-
 function WeatherStationIcy(log, config) {
+
     var that = this;
-    this.log = glog = log;
+    this.log = log;
     this.name = config.name;
     this.displayName = this.name;
     this.deviceId = config.deviceId;
-    this.interval = Math.min(Math.max(config.interval, 1), 60);
 
     this.config = config;
 
-    this.storedData = {};
-
-	this.alertLevel = config['alertLevel'];
-
+	alertLevel = config['alertLevel'];
+	
     this.setUpServices();
     
-    read();
+    this.readData();
+    
+   	fs.watch(readFile, (event, filename) => {
+   		if (event === 'change') this.readData();
+   	});
+};
 
-	intervalID = setInterval(function() {
+
+WeatherStationIcy.prototype.readData = function () {
+
+	var data = fs.readFileSync(readFile, "utf-8");
+	var lastSync = Date.parse(data.substring(0, 19));
+	if (readtime == lastSync) return;
+	readtime = lastSync;
+
+	temperature = parseFloat(data.substring(20));
+	battery = parseFloat(data.substring(57));
+
+    icy = temperature < alertLevel ? 1 : 0;
+
+	this.log("Icy data: ", temperature, icy, battery);
+	
+	if (icy != wasIcy) {
 		
-		var stats = fs.statSync(readFile);
-		
-		var doit = false;
-		if (ctime) {
-			if (ctime.getTime() != stats.mtime.getTime()) {
-				ctime = stats.mtime;
-				doit = true;
-			}
+		wasIcy = icy;
+	
+		this.fakeGatoHistoryService.addEntry({ time: moment().unix(), status: icy });
+	
+		this.iceAlertService.getCharacteristic(Characteristic.ContactSensorState).updateValue(icy, null);
+	
+		if (icy) {
+			this.timesOpened = this.timesOpened + 1;
+	        this.timeClose = this.timeClose + (moment().unix() - this.lastChange);
+	        this.lastActivation = moment().unix() - this.fakeGatoHistoryService.getInitialTime();
+		    this.iceAlertService.getCharacteristic(Characteristic.LastActivation).updateValue(this.lastActivation, null)
+	        this.iceAlertService.getCharacteristic(Characteristic.TimesOpened).updateValue(this.timesOpened, null)
 		}
 		else {
-			ctime = stats.mtime;
-			doit = true;
+	      	this.timeOpen = this.timeOpen + (moment().unix() - this.lastChange);
 		}
-			
-		if (doit) {
-			read();
-			glog("Ice Data: ", temperature, battery);
-
-			that.fakeGatoHistoryService.addEntry({
-				time: new Date().getTime() / 1000,
-				status: temperature < alertLevel ? 1 : 0
-				});
-		}
-	}, 2000);
-};
+	
+	    this.lastChange = moment().unix();
+	    this.fakeGatoHistoryService.setExtraPersistedData([{ "lastActivation": this.lastActivation, "lastReset": this.lastReset, "lastChange": this.lastChange, 
+	    													 "timesOpened": this.timesOpened, "timeOpen": this.timeOpen, "timeClose": this.timeClose }]);
+	}
+    this.batteryService.getCharacteristic(Characteristic.BatteryLevel).updateValue(null);
+    this.batteryService.getCharacteristic(Characteristic.StatusLowBattery).updateValue(null);
+}; 
 
 
 WeatherStationIcy.prototype.getFirmwareRevision = function (callback) {
-    callback(null, '1.0.0');
+    return callback(null, '1.0');
 };
 
 WeatherStationIcy.prototype.getBatteryLevel = function (callback) {
-	var perc = (battery - 0.8) * 100;
-    callback(null,perc);
+    return callback(null, (battery - 0.8) * 100);
 };
 
 WeatherStationIcy.prototype.getStatusActive = function (callback) {
-    callback(null, true);
+    return callback(null, true);
 };
 
 WeatherStationIcy.prototype.getStatusLowBattery = function (callback) {
-	if (battery >= 0.8)
-        callback(null, Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
-    else
-        callback(null, Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW);
+    return callback(null, battery >= 0.8 ? Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL : Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW);
 };
 
 WeatherStationIcy.prototype.getStatusIce = function (callback) {	
-    callback(null, temperature < alertLevel ? 
-    		 Characteristic.ContactSensorState.CONTACT_NOT_DETECTED : Characteristic.ContactSensorState.CONTACT_DETECTED);
+    return callback(null, icy);
 };
 
 
@@ -158,13 +152,13 @@ WeatherStationIcy.prototype.setReset = function (value, callback) {
 
 
 WeatherStationIcy.prototype.setUpServices = function () {
-    // info service
+
     this.informationService = new Service.AccessoryInformation();
 
     this.informationService
         .setCharacteristic(Characteristic.Manufacturer, "THN Systems")
-        .setCharacteristic(Characteristic.Model, "WeatherStationStormy")
-        .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + this.name)
+        .setCharacteristic(Characteristic.Model, "WeatherStationIcy")
+        .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + this.name);
     this.informationService.getCharacteristic(Characteristic.FirmwareRevision)
         .on('get', this.getFirmwareRevision.bind(this));
         
@@ -175,7 +169,7 @@ WeatherStationIcy.prototype.setUpServices = function () {
     this.batteryService.getCharacteristic(Characteristic.StatusLowBattery)
         .on('get', this.getStatusLowBattery.bind(this));
 
-    this.iceAlertService = new Service.ContactSensor("stürmisch", "maxWind");
+    this.iceAlertService = new Service.ContactSensor("Eis", "ice");
     this.iceAlertService.getCharacteristic(Characteristic.ContactSensorState)
         .on('get', this.getStatusIce.bind(this));
     this.iceAlertService.getCharacteristic(Characteristic.StatusLowBattery)
@@ -183,67 +177,62 @@ WeatherStationIcy.prototype.setUpServices = function () {
     this.iceAlertService.getCharacteristic(Characteristic.StatusActive)
         .on('get', this.getStatusActive.bind(this));
 
-    this.fakeGatoHistoryService = new FakeGatoHistoryService("door", this, { storage: 'fs' });
-
     Characteristic.OpenDuration = function() {
     	 Characteristic.call(this, 'Time open', 'E863F118-079E-48FF-8F27-9C2605A29F52');
          this.setProps({
            format: Characteristic.Formats.UINT32,
            unit: Characteristic.Units.SECONDS,
-           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY, Characteristic.Perms.WRITE]
          });
          this.value = this.getDefaultValue();
     };
-    inherits(Characteristic.OpenDuration, Characteristic);
     Characteristic.OpenDuration.UUID = 'E863F118-079E-48FF-8F27-9C2605A29F52';  
+    inherits(Characteristic.OpenDuration, Characteristic);
 
     Characteristic.ClosedDuration = function() {
     	 Characteristic.call(this, 'Time closed', 'E863F119-079E-48FF-8F27-9C2605A29F52');
          this.setProps({
            format: Characteristic.Formats.UINT32,
            unit: Characteristic.Units.SECONDS,
-           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY, Characteristic.Perms.WRITE]
          });
          this.value = this.getDefaultValue();
     };
-    inherits(Characteristic.ClosedDuration, Characteristic);
     Characteristic.ClosedDuration.UUID = 'E863F119-079E-48FF-8F27-9C2605A29F52';  
+    inherits(Characteristic.ClosedDuration, Characteristic);
     
     Characteristic.LastActivation = function() {
     	 Characteristic.call(this, 'Last Activation', 'E863F11A-079E-48FF-8F27-9C2605A29F52');
          this.setProps({
            format: Characteristic.Formats.UINT32,
-           unit: Characteristic.Units.SECONDS,
-           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY, Characteristic.Perms.WRITE]
          });
          this.value = this.getDefaultValue();
     };
-    inherits(Characteristic.LastActivation, Characteristic);
     Characteristic.LastActivation.UUID = 'E863F11A-079E-48FF-8F27-9C2605A29F52';  
+    inherits(Characteristic.LastActivation, Characteristic);
 
     Characteristic.TimesOpened = function() {
-    	 Characteristic.call(this, 'times opened', 'E863F129-079E-48FF-8F27-9C2605A29F52');
+    	 Characteristic.call(this, 'TimesOpened', 'E863F129-079E-48FF-8F27-9C2605A29F52');
          this.setProps({
            format: Characteristic.Formats.UINT32,
-           //unit: Characteristic.Units.SECONDS,
-           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+           perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
          });
          this.value = this.getDefaultValue();
     };
-    inherits(Characteristic.TimesOpened, Characteristic);
     Characteristic.TimesOpened.UUID = 'E863F129-079E-48FF-8F27-9C2605A29F52';  
+    inherits(Characteristic.TimesOpened, Characteristic);
 
     Characteristic.ResetTotal = function() {
-    	 Characteristic.call(this, 'times opened', 'E863F112-079E-48FF-8F27-9C2605A29F52');
+    	 Characteristic.call(this, 'reset total', 'E863F112-079E-48FF-8F27-9C2605A29F52');
          this.setProps({
            format: Characteristic.Formats.UINT32,
-           //unit: Characteristic.Units.SECONDS,
-           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+           perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY, Characteristic.Perms.WRITE]
          });
          this.value = this.getDefaultValue();
     };
-    inherits(Characteristic.ResetTotal, Characteristic);
     Characteristic.ResetTotal.UUID = 'E863F112-079E-48FF-8F27-9C2605A29F52';  
+    inherits(Characteristic.ResetTotal, Characteristic);
     
     this.iceAlertService.addCharacteristic(Characteristic.LastActivation)
         .on('get', this.getLastActivation.bind(this));
@@ -256,12 +245,14 @@ WeatherStationIcy.prototype.setUpServices = function () {
     this.iceAlertService.addCharacteristic(Characteristic.ResetTotal)
         .on('get', this.getReset.bind(this))
         .on('set', this.setReset.bind(this));
+
+    this.fakeGatoHistoryService = new FakeGatoHistoryService("door", this, { storage: 'fs' });
         
     if (this.fakeGatoHistoryService.getExtraPersistedData() == undefined) {
     	this.lastActivation = 0;
     	this.lastReset = moment().unix() - moment('2001-01-01T00:00:00Z').unix();
     	this.lastChange = moment().unix();
-    	this.timeOpened = 0;
+    	this.timesOpened = 0;
     	this.timeOpen = 0;
     	this.timeClose = 0;
            
@@ -277,13 +268,12 @@ WeatherStationIcy.prototype.setUpServices = function () {
             this.timeClose = this.fakeGatoHistoryService.getExtraPersistedData()[0].timeClose;
         }
         
-    var CustomCharacteristic = {};
-    
+    this.log(this.fakeGatoHistoryService.isHistoryLoaded());
 };
 
 
 WeatherStationIcy.prototype.getServices = function () {
-    var services = [this.informationService, this.batteryService, this.fakeGatoHistoryService, this.iceAlertService];
+    var services = [this.informationService, this.batteryService, this.iceAlertService, this.fakeGatoHistoryService];
 
     return services;
 };
